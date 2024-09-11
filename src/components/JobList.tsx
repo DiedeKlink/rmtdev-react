8415d45922acf004108702480de7fe53f22b3bdc
DiedeKlink <diede@protonmail.com>
2024-09-11 22:18:55 +0200

    Return Array, Type Custom Hook Return val

--- src/components/JobList.tsx
+++ src/components/JobList.tsx
@@ -12,9 +12,9 @@ export function JobList({ jobItems, isLoading }: JobListProps) {
     <ul className="job-list">
       {isLoading && <Spinner />}
       {!isLoading &&
-        jobItems
-          .splice(0, 7)
-          .map((jobItem) => <JobListItem key={jobItem.id} jobItem={jobItem} />)}
+        jobItems.map((jobItem) => (
+          <JobListItem key={jobItem.id} jobItem={jobItem} />
+        ))}
     </ul>
   );
 }
--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
+import { JobItem } from "./types";
 
 export function useJobItems(searchText: string) {
-  const [jobItems, setJobItems] = useState([]);
-
+  const [jobItems, setJobItems] = useState<JobItem[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+
+  const jobItemsSliced = jobItems.slice(0, 7);
 
   useEffect(() => {
     if (!searchText) return;
@@ -21,5 +23,5 @@ export function useJobItems(searchText: string) {
     fetchData();
   }, [searchText]);
 
-  return { jobItems, isLoading };
+  return [jobItemsSliced, isLoading] as const;
 }
