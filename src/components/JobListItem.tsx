ffaf04b039fa3f8fff4244241fa84cfa4a6a08a5
DiedeKlink <diede@protonmail.com>
2024-09-11 20:08:14 +0200

    Special Types File

--- src/components/JobListItem.tsx
+++ src/components/JobListItem.tsx
@@ -1,14 +1,5 @@
+import { JobItem } from "../lib/types";
 import BookmarkIcon from "./BookmarkIcon";
-
-type JobItem = {
-  id: number;
-  badgeLetters: string;
-  title: string;
-  company: string;
-  daysAgo: number;
-  relavanceScore: number;
-  date: string;
-};
 
 type JobListItemProps = {
   jobItem: JobItem;
---
+++ src/lib/types.ts
@@ -0,0 +1,9 @@
+export type JobItem = {
+  id: number;
+  badgeLetters: string;
+  title: string;
+  company: string;
+  daysAgo: number;
+  relavanceScore: number;
+  date: string;
+};
