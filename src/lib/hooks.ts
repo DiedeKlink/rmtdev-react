789c29b49b2c29213722123b89779fecb4975d19
DiedeKlink <diede@protonmail.com>
2024-09-12 15:53:59 +0200

    Change Fetch To React-Query

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,25 +1,28 @@
 import { useState, useEffect } from "react";
 import { JobItem, JobItemExpanded } from "./types";
 import { BASE_API_URL } from "./constants";
+import { useQuery } from "@tanstack/react-query";
 
-export function useActiveId() {
-  const [activeId, setActiveId] = useState<number | null>(null);
+export function useJobItem(Id: number | null) {
+  const { data, isLoading } = useQuery(
+    ["job-item", Id],
+    async () => {
+      const response = await fetch(`${BASE_API_URL}/${Id}`);
+      const data = await response.json();
+      return data;
+    },
+    {
+      staleTime: 1000 * 60 * 60,
+      refetchOnWindowFocus: false,
+      retry: false,
+      enabled: Boolean(Id),
+      onError: (error) => {
+        console.error("An error occurred while fetching job item", error);
+      },
+    }
+  );
 
-  useEffect(() => {
-    const handleHashChange = () => {
-      const id = +window.location.hash.slice(1);
-      setActiveId(id);
-    };
-    handleHashChange();
-
-    window.addEventListener("hashchange", handleHashChange);
-
-    return () => {
-      window.removeEventListener("hashchange", handleHashChange);
-    };
-  }, []);
-
-  return activeId;
+  return { jobItem: data.jobItem, isLoading } as const;
 }
 
 export function useJobItems(searchText: string) {
@@ -46,25 +49,24 @@ export function useJobItems(searchText: string) {
   return { jobItemsSliced, isLoading, totalNumberOfResults } as const;
 }
 
-export function useJobItem(Id: number | null) {
-  const [jobItem, setJobItem] = useState<JobItemExpanded | null>(null);
-  const [isLoading, setIsLoading] = useState(false);
+export function useActiveId() {
+  const [activeId, setActiveId] = useState<number | null>(null);
 
   useEffect(() => {
-    const fetchJobItem = async () => {
-      if (!Id) return;
-      setIsLoading(true);
-
-      const response = await fetch(`${BASE_API_URL}/${Id}`);
-      const data = await response.json();
-      setIsLoading(false);
-
-      setJobItem(data.jobItem);
+    const handleHashChange = () => {
+      const id = +window.location.hash.slice(1);
+      setActiveId(id);
     };
-    fetchJobItem();
-  }, [Id]);
+    handleHashChange();
 
-  return { jobItem, isLoading } as const;
+    window.addEventListener("hashchange", handleHashChange);
+
+    return () => {
+      window.removeEventListener("hashchange", handleHashChange);
+    };
+  }, []);
+
+  return activeId;
 }
 
 export function useDebounce<T>(value: T, delay = 500): T {
