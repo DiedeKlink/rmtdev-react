0e83cc461da1d8012ca4e9fbfe247736f6d8544a
DiedeKlink <diede@protonmail.com>
2024-09-14 09:18:30 +0200

    Memoize Context Value

--- src/contexts/JobItemsContextProvider.tsx
+++ src/contexts/JobItemsContextProvider.tsx
@@ -69,19 +69,31 @@ export default function JobItemsContextProvider({
     setSortBy(newSortBy);
   };
 
+  const contextValue = useMemo(
+    () => ({
+      jobItems,
+      jobItemsSortedAndSliced,
+      isLoading,
+      totalNumberOfResults,
+      currentPage,
+      sortBy,
+      handleChangePage,
+      handleChangeSortBy,
+    }),
+    [
+      jobItems,
+      jobItemsSortedAndSliced,
+      isLoading,
+      totalNumberOfResults,
+      currentPage,
+      sortBy,
+      handleChangePage,
+      handleChangeSortBy,
+    ]
+  );
+
   return (
-    <JobItemsContext.Provider
-      value={{
-        jobItems,
-        jobItemsSortedAndSliced,
-        isLoading,
-        totalNumberOfResults,
-        currentPage,
-        sortBy,
-        handleChangePage,
-        handleChangeSortBy,
-      }}
-    >
+    <JobItemsContext.Provider value={contextValue}>
       {children}
     </JobItemsContext.Provider>
   );
