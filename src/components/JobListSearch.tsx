4454341f6b4c454dffac888216543933641551fe
DiedeKlink <diede@protonmail.com>
2024-09-15 11:21:22 +0200

    TypeError fix

--- src/components/JobListSearch.tsx
+++ src/components/JobListSearch.tsx
@@ -3,5 +3,7 @@ import JobList from "./JobList";
 
 export default function JobListSearch() {
   const { jobItemsSortedAndSliced, isLoading } = useJobItemsContext();
-  return <JobList jobItems={jobItemsSortedAndSliced} isLoading={isLoading} />;
+  return (
+    <JobList jobItems={jobItemsSortedAndSliced ?? []} isLoading={isLoading} />
+  );
 }
